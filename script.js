9616ef4b6cc4871c8e5fed74479517bf0a668329
unknown <sabu0550@gmail.com>
2022-10-19 11:23:07 +0200

    add SCRIPT JS

---
+++ script.js
@@ -0,0 +1,22 @@
+//KEEP DATA FOR THE 
+let DataTable = []
+
+let amount = document.getElementById('amount')
+let transaction = document.getElementById('transaction')
+
+//VARABLES
+let expense = 0;
+let income = 0;
+let total = 0;
+
+function isValid (value) {
+    if (!value) {
+        return false
+    }
+    return true;
+}
+
+//ADD FUNCTION EXPENSE
+function addExpense() {
+    
+}
